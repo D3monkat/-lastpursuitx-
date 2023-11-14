040daf4ef09034a76375a95af92b0822604048dd
D3monkat <122725955+D3monkat@users.noreply.github.com>
2023-11-13 22:38:02 -0600

    add shells

---
+++ [defaultmaps]/K4MB1-AllShellsSubPack/fxmanifest.lua
@@ -0,0 +1,13 @@
+lua54 'yes'
+
+fx_version 'bodacious'
+game 'gta5'
+
+this_is_a_map 'yes'
+
+files {
+      'allshellsbundle_k4mb1.ytyp'
+}
+
+data_file 'DLC_ITYP_REQUEST' 'allshellsbundle_k4mb1.ytyp'
+dependency '/assetpacks'
