25db2278b6e82265f0839dcfeb859e1c55c86670
D3monkat <122725955+D3monkat@users.noreply.github.com>
2023-11-16 21:36:33 -0600

    Update vehicles.lua

--- [qb]/qb-core/shared/vehicles.lua
+++ [qb]/qb-core/shared/vehicles.lua
@@ -12,6 +12,15 @@ local Vehicles = {
         type = 'automobile',   -- Vehicle type, refer here https://docs.fivem.net/natives/?_0x6AE51D4B & here https://docs.fivem.net/natives/?_0xA273060E
         shop = 'pdm',          -- Can be a single shop or multiple shops. For multiple shops for example {'shopname1','shopname2','shopname3'}
     },
+    {
+        model = 'asbo',        -- This has to match the spawn code of the vehicle
+        name = 'Asbo',         -- This is the display of the vehicle
+        brand = 'Maxwell',     -- This is the vehicle's brand
+        price = 4000,          -- The price that the vehicle sells for
+        category = 'compacts', -- Catgegory of the vehilce, stick with GetVehicleClass() options https://docs.fivem.net/natives/?_0x29439776AAA00A62
+        type = 'automobile',   -- Vehicle type, refer here https://docs.fivem.net/natives/?_0x6AE51D4B & here https://docs.fivem.net/natives/?_0xA273060E
+        shop = 'pdm',          -- Can be a single shop or multiple shops. For multiple shops for example {'shopname1','shopname2','shopname3'}
+    },
     { model = 'blista',        name = 'Blista',                        brand = 'Dinka',           price = 13000,   category = 'compacts',       type = 'automobile', shop = 'pdm' },
     { model = 'brioso',        name = 'Brioso R/A',                    brand = 'Grotti',          price = 20000,   category = 'compacts',       type = 'automobile', shop = 'pdm' },
     { model = 'club',          name = 'Club',                          brand = 'BF',              price = 8000,    category = 'compacts',       type = 'automobile', shop = 'pdm' },
