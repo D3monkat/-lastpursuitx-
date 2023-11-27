a3a013bee355fa428f9cf8d7e72e7255677258a9
D3monkat <122725955+D3monkat@users.noreply.github.com>
2023-11-26 23:35:48 -0600

    add mor emapping

---
+++ [defaultmaps]/strawberry_gardens_verpi/fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'bodacious'
+game 'gta5'
+
+author 'VerpiMoDz'
+description 'Strawberry Gardens'
+version '1.1.0'
+
+this_is_a_map 'yes'
+
+--RELEASED FOR FREE BY VERPIMODZ ON CFX.RE and https://gta5-mods.com -> Original Discord: https://discord.gg/PvSdpeW--
