8f60159479da11f27794703c57f3cfb481d74792
D3monkat <122725955+D3monkat@users.noreply.github.com>
2023-11-28 22:07:38 -0600

    Update config.lua

--- [qb]/[phone]/lb-radioapp/config.lua
+++ [qb]/[phone]/lb-radioapp/config.lua
@@ -2,13 +2,13 @@ Config = {}
 Config.AutoInstall = true -- if true, the app will automatically be installed on your phone
 Config.Locale = "en" -- locale to use, must be in locales.lua
 Config.VoiceScript = "pma-voice" -- pma-voice or saltychat
-Config.Framework = "esx" -- esx, qbcore, or none
+Config.Framework = "qbcore" -- esx, qbcore, or none
 Config.LockedChannels = {
     {
         range = { 450.0, 451.0 },
         jobs = {
             ["police"] = true,
-            ["sheriff"] = true,
+            ["bcso"] = true,
         }
     },
 }
