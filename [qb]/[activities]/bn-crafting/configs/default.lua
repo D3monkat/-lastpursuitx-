69fa83e9f41e995c85ef823049e6960793e08bc3
D3monkat <122725955+D3monkat@users.noreply.github.com>
2023-12-02 10:54:43 -0600

    Update default.lua

--- [qb]/[activities]/bn-crafting/configs/default.lua
+++ [qb]/[activities]/bn-crafting/configs/default.lua
@@ -8,44 +8,5 @@ Config.Default = {
             xp = 5, -- Amount of XP you get for crafting this item
             time = 1000 -- Time it takes to craft this item in milliseconds
         },
-        ["phone"] = {
-            ["rubber"] = 1,
-            ["aluminum"] = 1,
-            ["iron"] = 1,
-            ["plastic"] = 1,
-            xp = 5,
-            time = 1000
-        },
-    },
-    [2] = {
-        ["weapon_carbinerifle"] = {
-            ["rubber"] = 450,
-            ["aluminum"] = 500,
-            ["iron"] = 15,
-            ["plastic"] = 410,
-            xp = 5,
-            time = 1000
-        },
-        ["weapon_pistol50"] = {
-            ["rubber"] = 2,
-            ["aluminum"] = 3,
-            ["iron"] = 1,
-            ["plastic"] = 4,
-            xp = 5,
-            time = 1000
-        },
-        ["weapon_knife"] = {
-            ["aluminum"] = 150,
-            ["iron"] = 25,
-            xp = 5,
-            time = 1000
-        },
-        ["armor"] = {
-            ["rubber"] = 10,
-            ["iron"] = 50,
-            ["plastic"] = 250,
-            xp = 5,
-            time = 1000
-        }
     }
 }
