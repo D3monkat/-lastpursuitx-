6fa91037befdc417024a0a965312597ee6a7e1f4
computerwhiz1dev <148601208+computerwhiz1dev@users.noreply.github.com>
2023-12-08 19:37:31 -0500

    ems eup

---
+++ [clothing]/mega_ems/fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'cerulean'
+game 'gta5'
+
+files {
+    'mp_m_freemode_01_mega_ems.meta',
+    'mp_f_freemode_01_mega_ems.meta'
+
+}
+
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_m_freemode_01_mega_ems.meta'
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_f_freemode_01_mega_ems.meta'
