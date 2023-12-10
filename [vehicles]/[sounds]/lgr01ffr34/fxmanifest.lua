103c32162528cb116a1337a98342deb1490f905b
D3monkat <122725955+D3monkat@users.noreply.github.com>
2023-12-09 21:40:49 -0600

    add new engine sound

---
+++ [vehicles]/[sounds]/lgr01ffr34/fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version "adamant"
+game "gta5"
+files {
+
+	"audioconfig/*.dat151.rel",
+	"audioconfig/*.dat54.rel",
+	"sfx/**/*.awc"
+}
+
+data_file "AUDIO_GAMEDATA" "audioconfig/lgr01ffr34_game.dat"
+data_file "AUDIO_SOUNDDATA" "audioconfig/lgr01ffr34_sounds.dat"
+data_file "AUDIO_WAVEPACK" "sfx/dlc_lgr01ffr34"
+dependency '/assetpacks'
