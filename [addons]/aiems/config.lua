89d095e1d621efc75a8d524b135927bc0aaba0bd
computerwhiz1dev <148601208+computerwhiz1dev@users.noreply.github.com>
2023-12-10 14:56:15 -0500

    f

--- [addons]/aiems/config.lua
+++ [addons]/aiems/config.lua
@@ -6,8 +6,8 @@ Config.Locale = 'en'
 Config.Doktor = 2 -- Needs to be less then this amount online for it to work
 Config.Price = 2000
 Config.MoneyFormat = '$'
-Config.MythicProgbar = true -- Set to true if you have mythic_progbar, otherwise set to false
-Config.MythicNotify = true -- if you have Mythic Notify then set this to true, otherwise false for standard ESX notifications.
+Config.MythicProgbar = false -- Set to true if you have mythic_progbar, otherwise set to false
+Config.MythicNotify = false -- if you have Mythic Notify then set this to true, otherwise false for standard ESX notifications.
 
 doktorPed = {name = "Doktor Hulusi", icon = "CHAR_CALL911", model = "s_m_m_doctor_01", colour = 111}
 
