8fddc4118b558e978c6be2435ee34e53b0253e69
D3monkat <122725955+D3monkat@users.noreply.github.com>
2023-12-09 21:48:26 -0600

    add missing photos

--- [ox]/ox_inventory/data/shops.lua
+++ [ox]/ox_inventory/data/shops.lua
@@ -78,6 +78,8 @@ return {
 			{ name = 'phone', price = 1250 },
 			{ name = 'huntingbait', price = 150 },
 			{ name = 'ammo-sniper', price = 60 },
+			{ name = 'WEAPON_KNIFE', price = 200 },
+			{ name = 'weapon_huntingrifle', price = 1450 metadata = { registered = true }}
 			{ name = 'fishingrod', price = 45 },
 			{ name = 'fishingbait', price = 12 },
 		}, locations = {
