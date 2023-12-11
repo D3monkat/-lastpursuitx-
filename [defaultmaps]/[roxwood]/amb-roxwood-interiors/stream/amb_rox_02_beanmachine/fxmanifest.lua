1abde6a031934e0347427077d472c5db3791b8c6
D3monkat <122725955+D3monkat@users.noreply.github.com>
2023-12-11 01:05:34 -0600

    f

---
+++ [defaultmaps]/[roxwood]/amb-roxwood-interiors/stream/amb_rox_02_beanmachine/fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'cerulean'
+game 'gta5'
+
+description "AMB Roxwood: Beanmachineyo | Chunk 2 | ballsballs i love hot asians omhhghgggggg"
+
+this_is_a_map 'yes'
+
+files {
+    'data/audio/amb_rox_02_int_beanmachine_doorsfx_game.dat151.rel',
+    'data/meta/interiorproxies.meta',
+    'data/meta/timecycle_amb_rox_02_int_beanmachine_yellowshityo.xml'
+}
+
+data_file 'AUDIO_GAMEDATA' 'data/audio/amb_rox_02_int_beanmachine_doorsfx_game.dat'
+data_file 'INTERIOR_PROXY_ORDER_FILE' 'data/meta/interiorproxies.meta'
+data_file 'TIMECYCLEMOD_FILE'  'data/meta/timecycle_amb_rox_02_int_beanmachine_yellowshityo.xml'
---
+++ [defaultmaps]/amb-hospital/fxmanifest.lua
@@ -0,0 +1,32 @@
+fx_version 'bodacious'
+game 'gta5'
+author 'The Ambitioneers Team'
+description 'Central Hospital Interior'
+version '1.0.0'
+
+this_is_a_map 'yes'
+
+files{
+'gtxd.meta',
+'hospital_game.dat151.rel'
+
+} 
+data_file 'GTXD_PARENTING_DATA' 'gtxd.meta'
+data_file 'DLC_ITYP_REQUEST' 'stream/ex_int_office_01_dlc.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/ex_int_office_02c_dlc.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/ex_int_office_03_dlc.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/int_corporate.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/ba_int_01_ba.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/int_industrial.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/h4_prop_h4_potted.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/ch_dlc_int_02_ch.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/apa_int_mp_h_08.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/apa_int_mp_h_06.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/apa_int_mp_h_05.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/ex_int_office_03b_dlc.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/h4_dlc_int_01_h4.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/sf_dlc_int_03_sec.ytyp'
+
+data_file 'AUDIO_GAMEDATA' 'hospital_game.dat'
+
+dependency '/assetpacks'
