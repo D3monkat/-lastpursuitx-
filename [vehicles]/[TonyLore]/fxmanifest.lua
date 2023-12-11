a272826c249a9a60e8e176230ce9942d1fcc4a14
computerwhiz1dev <148601208+computerwhiz1dev@users.noreply.github.com>
2023-12-11 14:06:02 -0500

    l

--- [vehicles]/[TonyLore]/fxmanifest.lua
+++ [vehicles]/[TonyLore]/fxmanifest.lua
@@ -2,7 +2,7 @@
 fx_version 'cerulean'
 game 'gta5'
 
-author 'PLOKMJNB#8692'
+author 'PLOKMdsdJNB#8692'
 description 'A single resource car pack to eliminate the rest'
 repository 'https://github.com/PLOKMJNB/FiveM-Civ-Car-Pack'
 
