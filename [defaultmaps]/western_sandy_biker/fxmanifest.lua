107cf96c916eddf7524558ef0b811c0bb4522631
D3monkat <122725955+D3monkat@users.noreply.github.com>
2023-12-12 13:37:05 -0600

    f

---
+++ [defaultmaps]/western_sandy_biker/fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'bodacious'
+game 'gta5'
+lua54 "yes"
+
+author 'Western'
+description 'Pikebike'
+version '1.0.0'
+
+this_is_a_map 'yes'
+
+server_scripts {
+	"stream/western_pb_proxies.lua",
+}
+	
