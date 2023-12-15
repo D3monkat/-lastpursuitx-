34b258558412183765f6d4d105bec9e3a2c6b6c3
computerwhiz1dev <148601208+computerwhiz1dev@users.noreply.github.com>
2023-12-14 19:50:41 -0500

    f

--- [addons]/jim-recycle/config.lua
+++ [addons]/jim-recycle/config.lua
@@ -147,6 +147,7 @@ Config = {
 		"glass",
 		"bottle",
 		"can",
+		"ceramic",
 	},
 	RecycleAmounts = {
 		["Recycle"] = {
--- [qb]/[activities]/bn-crafting/configs/gang.lua
+++ [qb]/[activities]/bn-crafting/configs/gang.lua
@@ -19,7 +19,7 @@ Config.Gang = {
         },
     },
     [6] = {
-        ["weapon_smg"] = {
+        ["weapon_microsmg"] = {
             ["aluminum"] = 500,
             ["rubber"] = 400,
             ["iron"] = 100,
@@ -27,10 +27,140 @@ Config.Gang = {
             xp = 5,
             time = 5000
         },
-        ["twerks_candy"] = {
-            ["snikkel_candy"] = 5,
-            xp = 5,
+        ["weapon_ceramicpistol"] = {
+            ["ceramic"] = 100,
+            ["iron"] = 100,
+            ["plastic"] = 225,
+            xp = 3,
             time = 1000
+        }
+    },
+    [7] = {
+        ["weapon_ak74"] = {
+            ["aluminum"] = 500,
+            ["rubber"] = 400,
+            ["iron"] = 100,
+            ["plastic"] = 450,
+            xp = 5,
+            time = 5000
+        },
+        ["weapon_aks74"] = {
+            ["ceramic"] = 100,
+            ["iron"] = 100,
+            ["plastic"] = 225,
+            ["aluminum"] = 500,
+            xp = 3,
+            time = 1000
+        }
+    },
+    [8] = {
+        ["weapon_assaultsmg"] = {
+            ["aluminum"] = 600,
+            ["rubber"] = 500,
+            ["iron"] = 200,
+            ["plastic"] = 550,
+            xp = 5,
+            time = 5000
+        }
+    },
+    [9] = {
+        ["weapon_mac10"] = {
+            ["aluminum"] = 600,
+            ["rubber"] = 500,
+            ["iron"] = 200,
+            ["plastic"] = 550,
+            xp = 5,
+            time = 5000
+        }
+    },
+    [10] = {
+        ["weapon_militaryrifle"] = {
+            ["aluminum"] = 900,
+            ["rubber"] = 700,
+            ["iron"] = 400,
+            ["plastic"] = 750,
+            xp = 8,
+            time = 9000
+        },
+        ["weapon_machinepistol"] = {
+            ["aluminum"] = 700,
+            ["rubber"] = 500,
+            ["iron"] = 200,
+            ["plastic"] = 550,
+            xp = 7,
+            time = 9000
+        },
+        ["weapon_glock17c"] = {
+            ["aluminum"] = 400,
+            ["rubber"] = 300,
+            ["iron"] = 100,
+            ["plastic"] = 250,
+            xp = 7,
+            time = 9000
+        }
+    },
+    [11] = {
+        ["weapon_glock18c"] = {
+            ["aluminum"] = 600,
+            ["rubber"] = 500,
+            ["iron"] = 280,
+            ["plastic"] = 420,
+            xp = 8,
+            time = 9000
+        },
+        ["weapon_machinepistol"] = {
+            ["aluminum"] = 700,
+            ["rubber"] = 500,
+            ["iron"] = 200,
+            ["plastic"] = 550,
+            xp = 7,
+            time = 9000
+        }
+    },
+    [12] = {
+        ["weapon_mp5"] = {
+            ["aluminum"] = 800,
+            ["rubber"] = 600,
+            ["iron"] = 300,
+            ["plastic"] = 650,
+            xp = 8,
+            time = 8000
+        }
+    },
+    [13] = {
+        ["weapon_combatpdw"] = {
+            ["aluminum"] = 800,
+            ["rubber"] = 600,
+            ["iron"] = 300,
+            ["plastic"] = 650,
+            xp = 9,
+            time = 8000
+        },
+        ["weapon_marksmanpistol"] = {
+            ["aluminum"] = 500,
+            ["rubber"] = 300,
+            ["iron"] = 100,
+            ["plastic"] = 350,
+            xp = 8,
+            time = 8000
+        }
+    },
+    [15] = {
+        ["weapon_bullpuprifle_mk2"] = {
+            ["aluminum"] = 1000,
+            ["rubber"] = 800,
+            ["iron"] = 400,
+            ["plastic"] = 650,
+            xp = 8,
+            time = 10000
+        },
+        ["weapon_bullpupshotgun"] = {
+            ["aluminum"] = 900,
+            ["rubber"] = 600,
+            ["iron"] = 200,
+            ["plastic"] = 550,
+            xp = 8,
+            time = 10000
         }
     }
 }
