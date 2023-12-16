3bb6598e20e7e3115c89e636b7797da45f584c67
D3monkat <122725955+D3monkat@users.noreply.github.com>
2023-12-16 13:32:06 -0600

    f

---
+++ [defaultmaps]/kat-christmas/fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'kat'
+description 'Christmas Content Pack'
+version '1.0.0'
+
+this_is_a_map 'yes'
+
+data_file 'DLC_ITYP_REQUEST' 'stream/nve_christmas.ytyp'
