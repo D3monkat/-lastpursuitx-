85f16e6b09f50e7292403cdaa1c59a4bd1df9c1a
D3monkat <122725955+D3monkat@users.noreply.github.com>
2023-12-17 13:12:08 -0600

    Update config.lua

--- [addons]/jim-recycle/config.lua
+++ [addons]/jim-recycle/config.lua
@@ -157,8 +157,8 @@ Config = {
 		["Trade"] = {
 			{ amount = 1, itemGive = 1, Min = 1, Max = 1, },
 			{ amount = 10, itemGive = 1, Min = 5, Max = 25, },
-			{ amount = 100, itemGive = 6, Min = 25, Max = 35, },
-			{ amount = 1000, itemGive = 8, Min = 35, Max = 55, },
+			{ amount = 100, itemGive = 6, Min = 25, Max = 55, },
+			{ amount = 1000, itemGive = 8, Min = 85, Max = 100, },
 		},
 	}
 }
