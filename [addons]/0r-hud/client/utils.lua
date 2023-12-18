6ae7a9ff78223f76ef3f395007dadd08d30a3036
D3monkat <122725955+D3monkat@users.noreply.github.com>
2023-12-17 20:27:26 -0600

    Update utils.lua

--- [addons]/0r-hud/client/utils.lua
+++ [addons]/0r-hud/client/utils.lua
@@ -4,9 +4,7 @@ function Utils.Functions:CustomFuelExport()
         return ESX.Math.Round(GetVehicleFuelLevel(HUD.Data.Vehicle), 2)
     ]]
     --
-    -- return exports['cdn-fuel']:GetFuel(vehicle)
-    return Koci.Framework.Shared.Round(exports["cdn-fuel"]:GetFuel(self.data.vehicle.entity) or 0, 2)
-    -- return false
+    return false
 end
 
 local function SetVehicleCruiseControlState(state)
