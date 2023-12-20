3249f16277b8f6dbb5a853ff03e400ec3a504b3a
D3monkat <122725955+D3monkat@users.noreply.github.com>
2023-12-20 02:55:32 -0600

    f

--- [vehicles]/[bcso]/cfx-trclassic-pursuitmode/server/sv_webhook.lua
+++
@@ -1,9 +0,0 @@
-QBCore = exports["qb-core"]:GetCoreObject()
-
-RegisterNetEvent('cfx-trclassic-pursuitmode:server:public', function(class, streetOne, streetTwo)
-    local class = class
-    local player = QBCore.Functions.GetPlayer(source)
-    local first = player.PlayerData.charinfo.firstname
-    local last = player.PlayerData.charinfo.lastname
-    TriggerEvent('cfx-trclassic-pursuitmode:server:logs', class, first, last, streetOne, streetTwo)
-end)
