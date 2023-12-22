09aa92486a70995de12f0e07b456dc111271b694
D3monkat <122725955+D3monkat@users.noreply.github.com>
2023-12-21 20:51:46 -0600

    remove pager and fix ai doc

--- [addons]/hh_aidoc/config.lua
+++ [addons]/hh_aidoc/config.lua
@@ -3,7 +3,7 @@
 -------------------
 Config = {}
 
-Config.Doctor = 2 -- Minimum Amount of EMS to work
+Config.Doctor = 1 -- Minimum Amount of EMS to work
 Config.Price = 2000
 Config.ReviveTime = 20000  --in msec
 
