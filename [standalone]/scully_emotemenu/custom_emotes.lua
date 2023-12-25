c3959617749e61a79fffad3f60771619b7890655
D3monkat <122725955+D3monkat@users.noreply.github.com>
2023-12-25 16:29:09 -0600

    Update custom_emotes.lua

--- [standalone]/scully_emotemenu/custom_emotes.lua
+++ [standalone]/scully_emotemenu/custom_emotes.lua
@@ -3,7 +3,46 @@ return {
     Walks = {},
     Scenarios = {},
     Expressions = {},
-    Emotes = {},
+    Emotes = {
+        {
+            Label = 'Radio 2',
+            Command = 'radio2',
+            Animation = 'radio_chatter',
+            Dictionary = 'random@arrests',
+            Options = {
+                Flags = {
+                    Loop = true,
+                    Move = true,
+                },
+            },
+        },
+        {
+            Label = 'Radio Chest',
+            Command = 'radiochest',
+            Animation = 'chest_mic',
+            Dictionary = 'anim@cop_mic_pose_002',
+            Options = {
+                Flags = {
+                    Loop = true,
+                    Move = true,
+                },
+            },
+        },
+        {
+            Label = 'Earpiece',
+            Command = 'earpiece',
+            Animation = 'cellphone_call_listen_base',
+            Dictionary = 'cellphone@',
+            Options = {
+                Flags = {
+                    Loop = true,
+                    Move = true,
+                },
+            },
+        },
+
+
+    },
     PropEmotes = {},
     ConsumableEmotes = {},
     DanceEmotes = {},
