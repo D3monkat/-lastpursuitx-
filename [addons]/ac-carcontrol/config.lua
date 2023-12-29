0335e119b0f61b0f27701607f986f4c88677a01d
D3monkat <122725955+D3monkat@users.noreply.github.com>
2023-12-28 22:37:28 -0600

    Update config.lua

--- [addons]/ac-carcontrol/config.lua
+++ [addons]/ac-carcontrol/config.lua
@@ -1,7 +1,7 @@
 Config = {
     Menu = {
         Command = "carmenu",
-        Keybind = "F11",
+        Keybind = "-*/",
         Align = "bottom-center" -- top-center, bottom-center, top-left, top-right, bottom-left, bottom-right
     },
     AlarmDuration = 30000, -- Second x 10000
