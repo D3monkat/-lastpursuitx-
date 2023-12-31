5c5af375b12e3efa5edf9ef92ab9011cb8c953e2
D3monkat <122725955+D3monkat@users.noreply.github.com>
2023-12-31 01:23:40 -0600

    f

--- [addons]/easy_allowlist/sql/players_queue_priority.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS `players_queue_priority` (
-	`identifier` VARCHAR(80) NOT NULL COLLATE 'utf8mb4_general_ci',
-	`priority` INT(11) NOT NULL DEFAULT '0',
-	PRIMARY KEY (`identifier`) USING BTREE,
-	INDEX `identifier` (`identifier`) USING BTREE
-)
-COLLATE='utf8mb4_general_ci'
-ENGINE=InnoDB
-;
