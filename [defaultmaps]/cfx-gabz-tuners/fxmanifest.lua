122568e9a4c29e9603d777660c5dc0bfd6502c84
D3monkat <122725955+D3monkat@users.noreply.github.com>
2023-12-31 13:49:40 -0600

    f

--- [defaultmaps]/cfx-gabz-tuners/fxmanifest.lua
+++ [defaultmaps]/cfx-gabz-tuners/fxmanifest.lua
@@ -7,7 +7,7 @@ lua54 'yes'
 this_is_a_map 'yes'
 
 dependencies { 
-    '/server:4960',     -- ⚠️PLEASE READ⚠️; Requires at least SERVER build 4960.
+    '/server:4960',     -- ⚠️PLEASE READ⚠️; Requires 2at least SERVER build 4960.
     '/gameBuild:2545',  -- ⚠️PLEASE READ⚠️; Requires at least GAME build 2545.
     'cfx-gabz-mapdata', -- ⚠️PLEASE READ⚠️; Requires [cfx-gabz-mapdata] to work properly.
 }
