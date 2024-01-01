0f3500cacd300763961a55c667414b550433ee6b
D3monkat <122725955+D3monkat@users.noreply.github.com>
2024-01-01 13:13:24 -0600

    f

---
+++ [vehicles]/[sounds]/ml720v8eng/fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version "adamant"
+game "gta5"
+
+files {
+	"audioconfig/*.dat151.rel",
+	"audioconfig/*.dat54.rel",
+	"audioconfig/*.dat10.rel",
+	"sfx/**/*.awc"
+}
+
+data_file "AUDIO_SYNTHDATA" "audioconfig/ml720v8eng_amp.dat"
+data_file "AUDIO_GAMEDATA" "audioconfig/ml720v8eng_game.dat"
+data_file "AUDIO_SOUNDDATA" "audioconfig/ml720v8eng_sounds.dat"
+data_file "AUDIO_WAVEPACK" "sfx/dlc_ml720v8eng"
