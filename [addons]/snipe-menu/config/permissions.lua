d28a18a44d0b9727c86f54e17ffc72e64bb35839
D3monkat <122725955+D3monkat@users.noreply.github.com>
2024-01-01 21:40:24 -0600

    Update permissions.lua

--- [addons]/snipe-menu/config/permissions.lua
+++ [addons]/snipe-menu/config/permissions.lua
@@ -28,8 +28,10 @@ Config.GodRoles = {
 Config.Permissions = {
     ["fivem:1651474"] = "god", -- the role god, admin or mod should be the key from Config.GodRoles which means the values that are added in square brackets.
     -- ["fivem:8420016"] = "god", -- the role god, admin or mod should be the key from Config.GodRoles which means the values that are added in square brackets.
-    ["fivem:601252"] = "god", ---Tony
+    ["fivem:601252"] = "god", ---Tony license:987a822c2c37b741fe7730121a6b3ce0c3f33de3
+
     ["license:a0d50cfdb39e0f75bef7ac811598de557281396e"] = "god",
+    ["license:987a822c2c37b741fe7730121a6b3ce0c3f33de3"] = "god",
     ["license:30d812d5f5281e72065ab29b21986e503270a667"] = "god", -- discord roles (copy the role id and paste it here) (REQUIRES badger_discord_api)
     ["license:68c5e90217783f07ca806058ee79805aa13a4a0c"] = "mod", -- citizenid for qbcore
     ["license:5969458e78a69dba91d29c5a36a62fb56e11a13f"] = "god", -- charid for ESX
