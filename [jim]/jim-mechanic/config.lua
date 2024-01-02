794300e8fc1d76dba8d304de9dae513741ea13a5
Jucid <jucidvibe@gmail.com>
2024-01-02 15:35:16 -0600

    fixed tunershop stashes hopefully

--- [jim]/jim-mechanic/config.lua
+++ [jim]/jim-mechanic/config.lua
@@ -22,7 +22,7 @@ Config = {
 		JobLocationRequired = true, -- Are the mecahanics locked to specific locations?
 		LocationBlips = true, 		-- Enable to grab the blip locations from locations.lua
 		CosmeticsJob = false, 		-- Do vehicle cosmetics require a mechanic job role?
-		JobRoles = {"shoplux", "redline", "rexmech", "blueline" }, -- These are the job roles who can use items if RequiresJob is enabled.
+		JobRoles = {"shoplux", "redline", "rexmech", "blueline","tuner" }, -- These are the job roles who can use items if RequiresJob is enabled.
 									-- Add your extra job roles here or just leave as "mechanic"
 									-- Example-- --JobRoles = { "mechanic", "tuner" }
 	},
