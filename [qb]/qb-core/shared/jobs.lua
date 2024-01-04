d266d16c09e619291618979a977abaea32b2bf83
computerwhiz1dev <148601208+computerwhiz1dev@users.noreply.github.com>
2024-01-04 11:45:46 -0500

    Update jobs.lua

--- [qb]/qb-core/shared/jobs.lua
+++ [qb]/qb-core/shared/jobs.lua
@@ -262,6 +262,15 @@ QBShared.Jobs = {
 			['100'] = { name = 'Showroom Sales', payment = 150 },
 		},
 	},
+	shopkatsmotors = {
+		label = 'Vehicle Dealer Kats',
+		defaultDuty = true,
+		offDutyPay = false,
+		grades = {
+			['0'] = { name = 'Recruit', payment = 50 },
+			['100'] = { name = 'Showroom Sales', payment = 150 },
+		},
+	},
 	shoptesla = {
 		label = 'Electric Vehicle Dealer',
 		defaultDuty = true,
