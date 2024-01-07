404908b19ac6355052c2c970d506a10c351856d7
computerwhiz1dev <148601208+computerwhiz1dev@users.noreply.github.com>
2024-01-07 09:48:25 -0500

    f

---
+++ [defaultmaps]/universestore_party_kawaii/fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'bodacious'
+game 'gta5'
+
+author 'Universe  Store https://discord.gg/J6fdzT55ks'
+
+this_is_a_map 'yes'
+
+
+data_file 'DLC_ITYP_REQUEST' 'stream/universestore_niver_01.ytyp'
+
+
+
+
+dependency '/assetpacks'
