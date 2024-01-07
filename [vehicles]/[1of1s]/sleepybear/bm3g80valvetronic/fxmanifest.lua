6ecfcc08fa2a8d47dd5c1fefa5a65abf6f954926
GooseGCRP <goosegaming16@gmail.com>
2024-01-06 18:18:35 -0600

    sleepybear

---
+++ [vehicles]/[1of1s]/sleepybear/bm3g80valvetronic/fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+files {
+  'audioconfig/bm3g80valvetronic_game.dat151.rel',
+  'audioconfig/bm3g80valvetronic_sounds.dat54.rel',
+  'sfx/dlc_bm3g80valvetronic/bm3g80valvetronic.awc',
+  'sfx/dlc_bm3g80valvetronic/bm3g80valvetronic_npc.awc'
+}
+
+data_file 'AUDIO_GAMEDATA' 'audioconfig/bm3g80valvetronic_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/bm3g80valvetronic_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_bm3g80valvetronic'
