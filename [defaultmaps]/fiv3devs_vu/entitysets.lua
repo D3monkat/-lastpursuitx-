1188e8604b6dc47217e46bef5fbd1f33525e4482
computerwhiz1dev <148601208+computerwhiz1dev@users.noreply.github.com>
2024-01-07 01:36:46 -0500

    Update entitysets.lua

--- [defaultmaps]/fiv3devs_vu/entitysets.lua
+++ [defaultmaps]/fiv3devs_vu/entitysets.lua
@@ -9,28 +9,28 @@ local vu_vanilla = GetInteriorAtCoords(113.652557, -1300.96277, 37.40684)
 	-- example: vu_red4 enabled means that in the "red" room entityset "hospital" will be enabled
 	
 	-- "red" room
-	EnableInteriorProp(vu_vanilla, "vu_red1")
-	DisableInteriorProp(vu_vanilla, "vu_red2")
+	DisableInteriorProp(vu_vanilla, "vu_red1")
+	EnableInteriorProp(vu_vanilla, "vu_red2")
 	DisableInteriorProp(vu_vanilla, "vu_red3")
 	DisableInteriorProp(vu_vanilla, "vu_red4")
 	
 	-- "pink" room
 	DisableInteriorProp(vu_vanilla, "vu_pink1")
-	EnableInteriorProp(vu_vanilla, "vu_pink2")
+	DisableInteriorProp(vu_vanilla, "vu_pink2")
 	DisableInteriorProp(vu_vanilla, "vu_pink3")
-	DisableInteriorProp(vu_vanilla, "vu_pink4")
+	EnableInteriorProp(vu_vanilla, "vu_pink4")
 	
 	-- "purple" room
-	DisableInteriorProp(vu_vanilla, "vu_purple1")
+	EnableInteriorProp(vu_vanilla, "vu_purple1")
 	DisableInteriorProp(vu_vanilla, "vu_purple2")
-	EnableInteriorProp(vu_vanilla, "vu_purple3")
+	DisableInteriorProp(vu_vanilla, "vu_purple3")
 	DisableInteriorProp(vu_vanilla, "vu_purple4")
 	
 	-- "blue" room
 	DisableInteriorProp(vu_vanilla, "vu_blue1")
 	DisableInteriorProp(vu_vanilla, "vu_blue2")
-	DisableInteriorProp(vu_vanilla, "vu_blue3")
-	EnableInteriorProp(vu_vanilla, "vu_blue4")
+	EnableInteriorProp(vu_vanilla, "vu_blue3")
+	DisableInteriorProp(vu_vanilla, "vu_blue4")
 	
 	-- EnableInteriorProp means an entityset is ENABLED
 	-- DisableInteriorProp means an entityset is DISABLED
