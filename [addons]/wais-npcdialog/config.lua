068be92d0a2feebcf819f3e06cbf19c31603ae08
D3monkat <122725955+D3monkat@users.noreply.github.com>
2024-01-07 22:37:29 -0600

    f

--- [addons]/wais-npcdialog/config.lua
+++ [addons]/wais-npcdialog/config.lua
@@ -119,7 +119,66 @@ Config.Peds = {
 
 
         end
-    }
+    },
+    [2] = {
+      ["model"] = "a_c_cat_01",
+      ["coords"] = vector4(-584.19, -1060.57, 23.37, 271.22),
+      ["cam"] = vector4(-583.38, -1060.39, 22.34, 90.81), -- If the camera angle doesn't look right, give the heading a full negative value, e.g. -93.0 instead of 93.0. If this doesn't work, enter the heading value of the direction your pad is facing the npc!
+      ["markerCoord"] = vector3(-584.19, -1060.57, 23.37),
+      ["interactive"] = {
+          -- ["type"] = "target", -- or fivem keys https://docs.fivem.net/docs/game-references/controls/
+          ["type"] = 38,-- https://docs.fivem.net/docs/game-references/controls/
+
+          ["key_label"] = "e", -- If type fivem is converted to index key, the name of the key must be entered "E"
+          ["text"] = "Talk to local", -- Text that will appear when you approach the npc
+          ["icon"] = "fa-solid fa-people-arrows",
+          ["distance"] = 2, -- Interactive distance
+          
+          ["uiMarker"] = true, -- If you make it True, you will have a nice image on the screen, but I do not recommend it for resmon.
+          ["uiDrawText"] = true, -- If you make it True, you will have a nice text on the screen, but I do not recommend it for resmon.
+
+          ["drawmarker_distance"] = 4,
+          ["interactiveState"] = false, -- -- Don't touch this
+          ["drawmarker_math"] = 46 -- It is part of a division process that magnifies the marker according to proximity and distance.
+      },  
+      ["modal_style"] = "question", -- warning, error, success, question or np
+      ["animDict"] = "amb@lo_res_idles@",
+      ["animName"] = "creatures_world_cat_standing_lo_res_base",
+      ["name"] = { -- Ped name
+      ["firstname"] = "Smokey", 
+      ["lastname"] = "The cat",
+      },
+      ["title"] = "meow meow", -- the text you want to appear maybe character task etc.
+      ["question"] = "Meow, meow meow meow meow meow? Meow meow meow meow meow!", -- question or text
+          ["options"] = {
+          ["option1"] = {
+              ["button"] = 1, -- A, B, C, D or 1, 2, 3, 4, 
+              ["label"] = "Is your owner arround?", -- The answer to the option will appear in the person
+              ["funcion_name"] = "catcafe", -- Function name
+              ["selected"] = false, -- Don't touch this
+          },        
+      },       
+      catcafe = function()
+          QBCore.Functions.Progressbar("Fiddle", "It seems the cat understood", 6500, false, true, {
+          disableMovement = false,
+          disableCarMovement = false,
+          disableMouse = false,
+          disableCombat = true,
+       }, {
+          flags = 49,
+       }, {}, {}, function() -- Done
+         
+         -- TriggerServerEvent("InteractSound_SV:PlayOnSource", "BellStartEnd", 0.7)
+         TriggerServerEvent("InteractSound_SV:PlayWithinDistance", 5, "BellStartEnd", 0.7)
+       end, function() -- Cancel
+          QBCore.Functions.Notify("You chicken...", "error", 4500)
+       end)
+
+
+
+
+      end
+  },
 }
 
 
---
+++ [defaultmaps]/nopixel4_3dp_mosley/fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Denis3D, Synced3D and sanhje'
+description 'Mosley'
+version '1.0.0'
+lua54 'yes'
+this_is_a_map 'yes'
+
+data_file 'TIMECYCLEMOD_FILE' '3dp_mosley_timecycles.xml'
+data_file 'TIMECYCLEMOD_FILE' '3dp_mosley_mechanic_timecycles.xml'
+
+files {
+    '3dp_mosley_timecycles.xml',
+    '3dp_mosley_mechanic_timecycles.xml'
+}
+
+dependency '/assetpacks'
