f5c2366a931884c60a9f0228f23519d1b87271de
GoosePX <goosegaming16@gmail.com>
2024-01-08 13:35:17 -0600

    Update locations.lua

--- [jim]/jim-mechanic/shared/locations.lua
+++ [jim]/jim-mechanic/shared/locations.lua
@@ -201,6 +201,7 @@ Config.Locations[#Config.Locations+1] = { --[[ RFC LS CUSTOMS ]]--
     zones = {
         vector2(8.903, -1664.435),
         vector2(-2.948, -1655.289),
+        vector2(-13.096, -1651.391),
         vector2(-26.549, -1670.801),
         vector2(-36.086, -1678.162),
         vector2(-47.601, -1668.066),
