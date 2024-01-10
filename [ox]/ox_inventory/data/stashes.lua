7ffe74a0828b0bc8eb80ef91901963db8ac273d2
computerwhiz1dev <148601208+computerwhiz1dev@users.noreply.github.com>
2024-01-10 14:51:30 -0500

    f

---
+++ [ox]/ox_inventory/data/stashes.lua
@@ -0,0 +1,57 @@
+return {
+	{
+		coords = vector3(462.89, -999.33, 30.69),
+		target = {
+			loc = vector3(462.89, -999.33, 30.69),
+			length = 1.2,
+			width = 5.6,
+			heading = 0,
+			minZ = 29.49,
+			maxZ = 32.09,
+			label = 'Open personal locker'
+		},
+		name = 'policelocker',
+		label = 'Personal locker',
+		owner = true,
+		slots = 70,
+		weight = 70000,
+		groups = shared.police
+	},
+	{
+		coords = vector3(1841.83, 3680.17, 34.19),
+		target = {
+			loc = vector3(1841.83, 3680.17, 34.19),
+			length = 1.2,
+			width = 5.6,
+			heading = 0,
+			minZ = 29.49,
+			maxZ = 32.09,
+			label = 'Open personal locker'
+		},
+		name = 'policelocker',
+		label = 'Personal locker',
+		owner = true,
+		slots = 70,
+		weight = 70000,
+		groups = shared.police
+	},
+
+	{
+		coords = vec3(301.3, -600.23, 43.28),
+		target = {
+			loc = vec3(301.82, -600.99, 43.29),
+			length = 0.6,
+			width = 1.8,
+			heading = 340,
+			minZ = 43.34,
+			maxZ = 44.74,
+			label = 'Open personal locker'
+		},
+		name = 'emslocker',
+		label = 'Personal Locker',
+		owner = true,
+		slots = 70,
+		weight = 70000,
+		groups = {['ambulance'] = 0}
+	},
+}
